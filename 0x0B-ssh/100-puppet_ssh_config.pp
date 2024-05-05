3fa596e576b9b128ef71a5eaf0574172468db85e
ejirocastro <awenayericastrop@gmail.com>
2024-05-05 16:58:23 -0700

    Advance Task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#puppet script to create ssh
+
+file line {'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   =>  '  passwordAuthentication no',
+}
+
+file_line {'decine identity file',
+  ensure => "present",
+  path   => '/etc/ssh/ssh_config',
+  line   => '  identityile ~/.ssh/scool',
+}
