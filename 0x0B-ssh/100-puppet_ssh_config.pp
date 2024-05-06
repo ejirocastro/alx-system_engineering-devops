c0925ac4332bcf8a0197badb69942ab10e4d9754
ejirocastro <awenayericastrop@gmail.com>
2024-05-05 17:55:27 -0700

    TASK 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,11 @@
 file_line {'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   =>  'passwordAuthentication no',
+  line   =>  '    passwordAuthentication no',
 }
 
 file_line {'Declare identity file':
   ensure => "present",
   path   => '/etc/ssh/ssh_config',
-  line   => 'identityile ~/.ssh/scool',
+  line   => '    identityile ~/.ssh/scool',
 }
